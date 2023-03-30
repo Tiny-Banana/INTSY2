b1e3b92abe8aa61f61c9d5d350b7ccc69592d706
gwnrocs <gwen_roco@dlsu.edu.ph>
2023-03-31 00:43:23 +0800

    add symptoms

--- test.pl
+++ test.pl
@@ -65,11 +65,49 @@ related(A, B) :-
     (pneumonia(P), member(A, P), member(B, P));
     (tubercolosis(T), member(A, T), member(B, T)).
 
-/*Initalization*/
-bronchitis(['have shortness of breath', 'persitent cough', 'wheeze', 'have chest pain', 'have fatigue', 
-            'have cold/flu in the past', 'have low grade fever']).
-pneumonia(['have blood in sputum', 'have high grade fever', 'have yellow/green sputum', 'have fatigue']).
-tubercolosis(['have low grade fever', 'have blood in sputum', 'have fatigue', 'have yellow/green sputum', 
-              'have sudden weight loss', 'have tubercolosis in the past', 'have exposure to a tubercolosis positive individual']).
+/*Initialization*/
+
+/* RESPIRATORY DISEASES */
+tuberculosis(['have night sweats', 'have shortness of breath', 'have low grade fever (<38.8C) for more than a week', 'have fatigue', 
+              'have persistent cough with yellow or green sputum for more than 2 weeks', 'have blood in sputum', 'have chest pain',
+              'have sudden weight loss', 'have a history of tuberculosis', 
+              'have a history of exposure to a tuberculosis positive individual recently']).
+
+pneumonia(['have shortness of breath', 'have fatigue', 'have high grade fever (>=38.8C)', 'have chest pain', 
+            'experience confusion', 'have persistent cough with yellow or green sputum for more than 2 weeks', 'have blood in sputum']).
+
+bronchitis(['have shortness of breath', 'have low grade fever (<38.8C)', 'have fatigue', 'have sore throat', 
+            'have cold symptoms such as mild headache or body ache', 'have a cold or flu', 'have persistent cough']).
+
+cvd(['have shortess of breath', 'have fast heartbeat', 'have slow heartbeat', 
+     'have pain / weakness / numbness in your upper / lower extremities', 'have feelings of loghtheadedness', 'have fatigue', 
+     'have swollen limbs', 'have a consistent blood pressure of systolic 130 mm Hg or more, and diastolic 80 mm Hg or more', 
+     'have a family history of heart disease', 'have obesity', 'frequently intake alcohol', 'frequently use tobacco', 'exercise / walk regularly'])
+
+/* OTHERS */
+dengue(['have gastrointestinal symptoms such as abdominal / belly pain , jaundice and others',
+        'have fever with either one of the following: eye pain, muscle pain, bone pain, joint pain, fever, headache, nausea / vomiting, and rash',
+        'experience vomiting at least 3 times in 24 hours', 'experience vomiting with blood', 'have bloody stool', 
+        'feel tired / restless / irritable'])
+
+hypertension(['have a consistent blood pressure of systolic 130 mm Hg or more, and diastolic 80 mm Hg or more'])
+
+/* DISEASES RELATED TO THE STOMACH / LIVER */
+typhoidfever(['have bloody stool', 'experience confusion', 'have an attention deficit', 'experience nosebleeds', 'have severe fatigue', 
+              'have high grade fever (>=38.8C)', 'have stomach ache', 'have diarrhea', 'have loss of appetite'])
+
+hepatitisA(['have jaundice', 'have loss of appetite', 'have stomach ache', 'have diarrhea', 'have dark (brown) urine', 'have light colored stool'
+            'have fatigue', 'have high grade fever (>=38.8C)'])
+
+leptospirosis(['have high grade fever (>=38.8C)', 'have headaches', 'have muscle aches', 'experience vomiting', 'have fatigue', 'have diarrhea',
+               'have rashes', 'have stomach ache', 'have jaundice', 'have red eyes'])
+
+helminthiasis(['have diarrhea', 'have abdominal pain', 'have chronic symptoms of malnutrition', 'have fatigue', 'have rectal prolapse',])
+
+cholera(['have diarrhea', 'experience vomiting', 'have leg cramps', 'experience dehydration', 'experience loss of skin elasticity',
+         'have rapid heart rate', 'have a dry mouth', 'experience thirst', 'have fatigue', 'experience restlessness', 
+         'have low blood pressure'])
+
+
 askedsymp([]).
 has([]).
